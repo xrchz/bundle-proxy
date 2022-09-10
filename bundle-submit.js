2da6225a691e81c3961315c3de9b4ee7cb54436e
Ramana Kumar <ramana@member.fsf.org>
2022-09-10 22:30:18 +0000

    Remove unused fee stuff

--- bundle-submit.js
+++ bundle-submit.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 const { program } = require('commander')
-program.option('-i, --maxPrioFee <gwei>', 'maximum priority fee', '2')
-       .option('-f, --futureBlocks <n>', 'number of blocks after current to target', '1')
+program.option('-f, --futureBlocks <n>', 'number of blocks after current to target', '1')
        .option('-c, --network <name>', 'network (chain)', 'mainnet')
        .option('-t, --txFile <file>', 'file containing lines of signed transactions', 'txs.txt')
        .option('-n, --dryRun', 'simulate bundle only')
 program.parse()
 const options = program.opts()
+
+const futureBlocks = parseInt(options.futureBlocks)
 
 const bundle = require('fs')
   .readFileSync(options.txFile, 'utf-8')
@@ -14,10 +15,6 @@ const bundle = require('fs')
   .map(tx => ({signedTransaction: tx}))
 console.log(`Bundle:\n${JSON.stringify(bundle,null,2)}`)
 const ethers = require('ethers')
-
-const GWEI = ethers.BigNumber.from(10).pow(9)
-const PRIORITY_FEE = GWEI.mul(options.maxPrioFee)
-const BLOCKS_IN_FUTURE = parseInt(options.futureBlocks)
 
 const flashbots = require('@flashbots/ethers-provider-bundle')
 
@@ -36,15 +33,14 @@ const flashbotsProvider = await flashbots.FlashbotsBundleProvider.create(
   options.network)
 
 const currentBlockNumber = await provider.getBlockNumber()
-const targetBlockNumber = currentBlockNumber + BLOCKS_IN_FUTURE
+const targetBlockNumber = currentBlockNumber + futureBlocks
 const currentBlock = await provider.getBlock(currentBlockNumber)
 const maxBaseFeeInFutureBlock = flashbots.FlashbotsBundleProvider.getMaxBaseFeeInFutureBlock(
-  currentBlock.baseFeePerGas, BLOCKS_IN_FUTURE)
+  currentBlock.baseFeePerGas, futureBlocks)
 
 console.log(`Current block number: ${currentBlockNumber}`)
 console.log(`Current base fee: ${currentBlock.baseFeePerGas}`)
 console.log(`Target block number: ${targetBlockNumber}`)
-console.log(`Max fee per gas: ${maxBaseFeeInFutureBlock.add(options.maxPrioFee).toString()} gwei`)
 
 if (options.dryRun) {
   const signedBundle = await flashbotsProvider.signBundle(bundle)
