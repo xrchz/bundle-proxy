1d4904ac4c81e8874633214f68e6142d93ea6186
Ramana Kumar <ramana@member.fsf.org>
2022-09-11 09:01:14 +0000

    Add option to override nonce

--- bundle-proxy.js
+++ bundle-proxy.js
@@ -3,6 +3,7 @@ const tiny = require('tiny-json-http')
 
 program.option('-p, --port <port>', 'port to listen on', '8549')
        .option('-r, --rpc <url>', 'URL of base RPC server', 'http://localhost:8545')
+       .option('--overrideNonce <nonce>', 'override transaction count requets to this nonce')
 program.parse()
 const options = program.opts()
 const url = options.rpc
@@ -14,15 +15,26 @@ app.use(express.json())
 
 app.post('/', async (req, res) => {
   const { jsonrpc, id, method, params } = req.body
-  if (method == 'eth_sendTransaction') {
+  if (method === 'eth_sendTransaction') {
     console.log(`Captured sendTransaction:\n${JSON.stringify(params)}`)
   }
-  else if (method == 'eth_sendRawTransaction') {
+  else if (method === 'eth_sendRawTransaction') {
     console.log(`Captured sendRawTransaction:\n${params[0]}`)
+  }
+  else if (options.overrideNonce && method === 'eth_getTransactionCount') {
+    // console.log(`Proxying data: ${JSON.stringify(req.body)}`)
+    const result = (await tiny.post({url, data:req.body}))
+    // console.log(`Received back: ${JSON.stringify(result.body)}`)
+    // console.log('Overriding result')
+    result.body.result = `0x${parseInt(options.overrideNonce).toString(16)}`
+    // console.log(`Proxying back: ${JSON.stringify(result.body)}`)
+    res.json(result.body)
   }
   else {
     // console.log(`Proxying data: ${JSON.stringify(req.body)}`)
-    res.json((await tiny.post({url, data:req.body})).body)
+    const result = (await tiny.post({url, data:req.body}))
+    // console.log(`Proxying back: ${JSON.stringify(result.body)}`)
+    res.json(result.body)
   }
 })
 
